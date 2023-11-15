e31e8af9dd83e834eea8ca9256a09fec003a23fb
Szymon Gross <szyg@netcompany.com>
2023-11-15 21:17:39 +0100

    Fixed prod build

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-
+console.log("Is in prod mode?" + environment.PRODUCTION);
 if (environment.PRODUCTION) {
   enableProdMode();
 }
