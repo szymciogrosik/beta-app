3679b44a96fddaccbef9f6d23a2563a369485334
Szymon Gross <szyg@netcompany.com>
2023-12-29 13:10:36 +0100

    Added copy button

--- src/app/_models/bibleperday/output/bible-per-day.ts
+++ src/app/_models/bibleperday/output/bible-per-day.ts
@@ -1,14 +1,15 @@
 import {Quote} from "../quote";
 import {SpecialOccasionContainer} from "./special-occasion-container";
 import {ContemplationContainer} from "./contemplation-container";
+import {QuoteContainer} from "../quote-container";
 
 export class BiblePerDay {
   date: Quote = new Quote();
-  firstStandardText: Quote = new Quote();
-  secondStandardText: Quote = new Quote();
-  firstAdditionalText: Quote = new Quote();
-  secondAdditionalText: Quote = new Quote();
-  quoteNotFromBibleText: Quote = new Quote();
+  firstStandardText: QuoteContainer = new QuoteContainer();
+  secondStandardText: QuoteContainer = new QuoteContainer();
+  firstAdditionalText: QuoteContainer = new QuoteContainer();
+  secondAdditionalText: QuoteContainer = new QuoteContainer();
+  quoteNotFromBibleText: QuoteContainer = new QuoteContainer();
   specialOccasionContainer: SpecialOccasionContainer = new SpecialOccasionContainer();
   contemplationContainer: ContemplationContainer = new ContemplationContainer();
 }
